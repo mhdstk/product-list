cf991e2bce5ef84a2214349b1db700d13b559594
mosh1331 <shamil.tfora@gmail.com>
2024-11-04 07:06:17 +0400

    products data update

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -3,113 +3,13 @@
 
 //@ts-ignore 
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import {products} from  '../utils/data'
 
 
 
-const products = [
-  {
-    id: 1,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/13367286/pexels-photo-13367286.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 2,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/8382689/pexels-photo-8382689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 3,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/6791447/pexels-photo-6791447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 4,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/19800760/pexels-photo-19800760/free-photo-of-modern-smartphone-in-studio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 5,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/518417/pexels-photo-518417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 6,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/22604171/pexels-photo-22604171/free-photo-of-touchscreen-of-smartphone.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 7,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/29020349/pexels-photo-29020349/free-photo-of-modern-smartphone-on-wooden-surface.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 8,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/11494897/pexels-photo-11494897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 9,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/9741343/pexels-photo-9741343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 10,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/13360476/pexels-photo-13360476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  {
-    id: 11,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc: 'https://images.pexels.com/photos/11216259/pexels-photo-11216259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  // Add more products as needed
-];
 
 export default function ProductList() {
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -121,7 +21,7 @@ export default function ProductList() {
               <div className="relative">
                 <div className="relative h-72 w-full overflow-hidden rounded-lg">
                   <img
-                    alt={product.imageAlt}
+                    alt={product.name}
                     src={product.imageSrc}
                     className="h-full w-full object-cover object-center"
                   />
@@ -129,6 +29,7 @@ export default function ProductList() {
                 <div className="relative mt-4">
                   <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
                   <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  <p className="mt-1 text-sm text-gray-500">{product.description}</p>
                 </div>
                 <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                   <div
@@ -140,7 +41,8 @@ export default function ProductList() {
               </div>
               <div className="mt-6">
                 <a
-                  href={product.href}
+                target='_blank'
+                  href={`https://wa.me/+919947496367/?text=${`Hi, i would like to enquire about ${product.name}`}`}
                   className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
                 >
                   <WhatsAppIcon /> Enquire Now<span className="sr-only">, {product.name}</span>
